1dc645e1e0378e545c263608e681faf05b8394b7
Tomas Vojtech <tomas.vojtech@gmail.com>
2018-03-25 19:43:47 +0200

    fix(search): Minor style changes.

--- src/components/search/SearchPage.js
+++ src/components/search/SearchPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { compose, withHandlers, lifecycle, withState } from 'recompose'
+import { compose, withHandlers, lifecycle, withState, withProps } from 'recompose'
 import { Box, Flex, Image } from 'rebass'
 import _fp from 'lodash/fp'
 import qs from 'query-string'
@@ -17,11 +17,11 @@ class SearchPage extends React.Component {
   }
 
   render() {
-    const { search, searchChange, performSearch, locations = [], selectedLocation, setSelectedLocation } = this.props;
+    const { searchText, searchTextChange, performSearch, locations = [], selectedLocation, setSelectedLocation } = this.props;
     return (
       <Box>
-        <Box ml="auto" width={[1, 1 / 2, 1 / 3, 1 / 4]}>
-          <SearchBar onSearch={performSearch} value={search} onValueChange={evt => searchChange(evt.target.value)} placeholder="Search city" inputRef={ref => (this.inputSearchRef = ref)} />
+        <Box width={1}>
+          <SearchBar onSearch={performSearch} value={searchText} onValueChange={evt => searchTextChange(evt.target.value)} placeholder="Search city" inputRef={ref => (this.inputSearchRef = ref)} />
         </Box>
         {locations.length > 0 &&
           <React.Fragment>
@@ -35,32 +35,28 @@ class SearchPage extends React.Component {
   }
 }
 
-const obtainSearchQueryParam = ({ location }) => qs.parse(location.search).search || ""
-
 const StatefulSearchPage = compose(
-  withState("search", "searchChange", obtainSearchQueryParam),
+  withProps(({ location }) => ({ search: qs.parse(location.search).search || "" })),
+  withState("searchText", "searchTextChange", ""),
   withState('selectedLocation', 'setSelectedLocation', undefined),
-  withAsyncData(({ location, search }) => ({
-    locations: () => {
-      if (!search) {
-        return
+  withAsyncData({
+    mapFetchToProps: ({ search }) => ({
+      locations: () => {
+        if (!search) {
+          return
+        }
+        return weatherApi.searchLocations(search).then(_fp.get("list"))
       }
-      return weatherApi.searchLocations(search).then(_fp.get("list"))
+    }),
+    shouldRefetch(nextProps, oldProps) {
+      if (nextProps.search !== oldProps.search) {
+        return "locations"
+      }
     }
-  })),
-  // fixme: after clicking on app logo search is not rerendered
-  // lifecycle({
-  //   componentWillReceiveProps(nextProps) {
-  //     if (nextProps.location.search !== this.props.location.search) {
-  //       this.props.searchChange(obtainSearchQueryParam({ location: this.props.location }))
-  //       this.props.refetch()
-  //     }
-  //   }
-  // }),
+  }),
   withHandlers({
-    performSearch: ({ history, location, refetch, search }) => () => {
-      history.push({ search: qs.stringify({ ...qs.parse(location.search), search }) })
-      refetch()
+    performSearch: ({ history, location, refetch, searchText }) => () => {
+      history.push({ search: qs.stringify({ ...qs.parse(location.search), search: searchText }) })
     }
   }
   )
