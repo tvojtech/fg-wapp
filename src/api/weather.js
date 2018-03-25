f2644debceba99ab0091214596e8914ee9d14e3e
Tomas Vojtech <tomas.vojtech@gmail.com>
2018-03-25 16:23:58 +0200

    fix: Change weather api code to work with Netlify.

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,9 +1,16 @@
 import request from 'superagent'
 import _fp from 'lodash/fp'
 
+const urlBase = "https://api.openweathermap.org/data/2.5"
+
 const weatherApiKey = '39d717bb99e1175215f4bcfa8f2cbb37'
 
-const executeWeatherGet = (url, params = {}) => request.get(url).query({ ...params, appid: weatherApiKey }).then(_fp.get("body"))
+let executeWeatherGet = (url, params = {}) => request.get(url).query({ ...params, appid: weatherApiKey }).then(_fp.get("body"))
+
+if (process.env.NODE_ENV !== "development") {
+  const oldExecuteWeatherGet = executeWeatherGet;
+  executeWeatherGet = (url, params) => oldExecuteWeatherGet(urlBase + url.replace("api/weather", ""), params)
+}
 
 export const searchLocations = location => executeWeatherGet(`api/weather/find`, { q: location })
 
