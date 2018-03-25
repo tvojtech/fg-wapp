f1e71f718b015f4f6c4c5bb46796167e39fd5c44
Tomas Vojtech <blaf@blaf-mbp.local>
2018-03-25 15:42:41 +0200

    feat(detail): Style detail page.

--- src/components/detail/DetailPage.js
+++ src/components/detail/DetailPage.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import { compose, withStateHandlers, lifecycle, branch, renderComponent } from 'recompose'
-import { Box, Flex, Heading } from 'rebass'
+import { Box, Flex, Heading, Image, Text } from 'rebass'
 import _fp from 'lodash/fp'
 import withAsyncData from '../withAsyncData'
 import Loading from '../Loading'
 import * as weatherApi from '../../api/weather'
-import TemperatureIndicator from './TemperatureIndicator'
 
-const DetailPage = ({ weather }) => (
+const sectionProps = { w: [1, null, 1 / 3], justify: "center", align: "center" }
+
+const DetailPage = ({ weather: { name, main, weather, wind } }) => (
   <Box>
-    <Heading>{weather.name}</Heading>
-    <Box>
-      <TemperatureIndicator weather={weather} />
-    </Box>
+    <Flex direction="row" justify={["center", null, "flex-start"]}>
+      <h1>{name}</h1>
+    </Flex>
+    <Flex wrap={true} direction="row" justify="space-between">
+      <Flex {...sectionProps}>
+        <Image src={`http://openweathermap.org/img/w/${weather[0].icon}.png`} width={1} />
+      </Flex>
+      <Flex {...sectionProps}>
+        <Text fontSize={8}>
+          {Math.round(main.temp)}&deg;C
+        </Text>
+      </Flex>
+      <Flex {...sectionProps}>
+        <Text fontSize={8}>{wind.speed}&nbsp;m/s</Text>
+      </Flex>
+    </Flex>
   </Box>
 )
 
--- src/components/detail/TemperatureIndicator.js
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-
-export default ({ weather: { main: { temp } } }) => <div>{temp}</div>
